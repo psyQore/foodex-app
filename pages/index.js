703b8fae58d41ba7b0782826c715fcccfba48134
Juan Galarce <j6alarce@gmail.com>
2022-08-30 13:21:27 -0400

    Se realiza la consulta a la bd de prisma con nextjs y se traen las categories

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+export default function Home({ categories }) {
+  console.log(categories);
   return (
     <div>
       <h1>NextJs</h1>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categories = await prisma.category.findMany();
+
+  return {
+    props: {
+      categories,
+    },
+  };
+};
