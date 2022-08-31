47200b361b2a8a096e540cdef2fdb456d38cc17d
Juan Galarce <j6alarce@gmail.com>
2022-08-31 08:29:25 -0400

    Se añade la funcionalidad para ajustar las cantidades del pedido

--- components/ModalProduct.jsx
+++ components/ModalProduct.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import useStore from "../hooks/useStore";
 import { formatQuantity } from "../helpers";
 
 const ModalProduct = () => {
   const { product, handleChangeModal } = useStore();
+  const [quantity, setQuantity] = useState(1);
 
   return (
     <div className="md:flex gap-10">
@@ -40,6 +41,56 @@ const ModalProduct = () => {
         <p className="mt-5 font-black text-5xl text-amber-500">
           {formatQuantity(product.price)}
         </p>
+
+        <div className="flex gap-4 mt-5">
+          <button
+            type="button"
+            onClick={() => {
+              if (quantity <= 1) return;
+              setQuantity(quantity - 1);
+            }}
+          >
+            <svg
+              class="w-7 h-7"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </button>
+
+          <p className="text-3xl">{quantity}</p>
+
+          <button
+            type="button"
+            onClick={() => {
+              if (quantity >= 10) return;
+              setQuantity(quantity + 1);
+            }}
+          >
+            <svg
+              className="w-7 h-7"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
