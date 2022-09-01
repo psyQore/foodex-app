50a4e3309c588b4d9ceae723aa5bef88d64a8a92
Juan Galarce <j6alarce@gmail.com>
2022-09-01 14:09:20 -0400

    Se añaden los botones de editar y eliminar en la pag de summary

--- components/SummaryProduct.jsx
+++ components/SummaryProduct.jsx
@@ -13,7 +13,7 @@ const SummaryProduct = ({ product }) => {
           src={`/assets/img/${product.imagen}.jpg`}
         />
       </div>
-      <div className="md:w-5/6">
+      <div className="md:w-4/6">
         <p className="text-3xl font-bold">{product.name}</p>
         <p className="text-xl font-bold mt-2">Cantidad: {product.quantity}</p>
         <p className="text-3xl font-bold text-amber-500 mt-2">
@@ -22,6 +22,49 @@ const SummaryProduct = ({ product }) => {
         <p className="text-sm text-gray-700 mt-2">
           Subtotal: {formatQuantity(product.price * product.quantity)}{" "}
         </p>
+      </div>
+      <div>
+        <button
+          className="bg-sky-700 flex gap-2 px-5 py-2 text-white rounded-md font-bold uppercase shadow-md w-full text-center"
+          type="button"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+          Editar
+        </button>
+
+        <button
+          className="bg-red-700 flex gap-2 px-5 py-2 text-white rounded-md font-bold uppercase shadow-md w-full text-center mt-3"
+          type="button"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+          Editar
+        </button>
       </div>
     </div>
   );
