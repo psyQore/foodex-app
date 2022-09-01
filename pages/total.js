adc58a4b25225285fe7253ad2a72828236293fea
Juan Galarce <j6alarce@gmail.com>
2022-09-01 15:38:58 -0400

    Se crea una funcion para evitar que el usuario envie el form vacio y no tenga un pedido

--- pages/total.js
+++ pages/total.js
@@ -1,12 +1,29 @@
+import { useEffect, useCallback } from "react";
 import Layout from "../layout/Layout";
+import useStore from "../hooks/useStore";
 
 export default function Total() {
+  const { order } = useStore();
+
+  const checkOrder = useCallback(() => {
+    return order.length === 0;
+  }, [order]);
+
+  useEffect(() => {
+    checkOrder();
+  }, [order]);
+
+  const putOrder = (e) => {
+    e.preventDefault();
+    console.log("Enviando Orden");
+  };
+
   return (
     <Layout page="Total y Confirmar Pedido">
       <h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
       <p className="text-2xl my-10">Confirma tu pedido a continuación</p>
 
-      <form>
+      <form onSubmit={putOrder}>
         <div>
           <label
             htmlFor="name"
@@ -27,8 +44,12 @@ export default function Total() {
         </div>
         <div className="mt-5">
           <input
-            className="bg-indigo-600 w-full text-center lg:w-auto px-5 py-2 rounded uppercase font-bold text-white hover:cursor-pointer"
+            type="submit"
+            className={` ${
+              checkOrder() ? "bg-indigo-300" : " bg-indigo-600 hover:cursor-pointer"
+            } w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center`}
             value="Confirmar Pedido"
+            disabled={checkOrder()}
           />
         </div>
       </form>
