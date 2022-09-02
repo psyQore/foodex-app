49ef23c00ba11819f9cbe9e89013465815492d4f
Juan Galarce <j6alarce@gmail.com>
2022-09-02 18:31:14 -0400

    Se inserta la orden creada en la bd

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,7 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+
   if (req.method === "POST") {
-    console.log(req.body)
-    res.json({ metodo: "POST!!" });
+    const order = await prisma.order.create({
+      data: {
+        name: req.body.name,
+        total: req.body.total,
+        order: req.body.order,
+        date: req.body.date,
+      },
+    });
+
+    res.json(order);
   }
 }
